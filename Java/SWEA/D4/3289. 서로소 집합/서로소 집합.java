8e6dc6c23d836ed32bcf14312cf3e89b253fc066
LeeJungHyeok <wndgur76@naver.com>
2024-08-28 11:59:51 +0900

    [D4] Title: 서로소 집합, Time: 670 ms, Memory: 102,936 KB -BaekjoonHub

--- Java/SWEA/D4/3289. 서로소 집합/서로소 집합.java
+++ Java/SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -4,51 +4,56 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static int[] parents;
 	
 	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int n, m, cmd, a, b;
+		
 		for (int tc = 1; tc <= T; tc++) {
 			sb.append('#').append(tc).append(' ');
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
+			
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
 			
 			parents = new int[n];
-			for (int i = 0; i < n; i++)
-				parents[i] = i;
+			for (int i = 0; i < n; i++) parents[i] = i;
 			
 			for (int i = 0; i < m; i++) {
 				st = new StringTokenizer(br.readLine());
-				int cmd = Integer.parseInt(st.nextToken());
-				int a = Integer.parseInt(st.nextToken())-1;
-				int b = Integer.parseInt(st.nextToken())-1;
+				cmd = Integer.parseInt(st.nextToken());
+				a = Integer.parseInt(st.nextToken())-1;
+				b = Integer.parseInt(st.nextToken())-1;
 				if(cmd == 0) union(a, b);
-				else if(cmd == 1) sb.append(find(a, b));
+				else sb.append(find(a, b));
 			}
+			
 			sb.append('\n');
 		}
 		System.out.println(sb.toString());
 	}
 	
 	public static void union(int a, int b) {
-		int r1 = find(a);
-		int r2 = find(b);
+		int r1 = findSet(a);
+		int r2 = findSet(b);
 		if(r1==r2) return;
 		parents[r2] = r1;
 	}
 	
 	public static int find(int a, int b) {
-		int r1 = find(a);
-		int r2 = find(b);
+		int r1 = findSet(a);
+		int r2 = findSet(b);
 		return r1==r2?1:0;
 	}
 	
-	public static int find(int a) {
+	public static int findSet(int a) {
 		if(parents[a] == a) return a;
-		return parents[a] = find(parents[a]);
+		return parents[a] = findSet(parents[a]);
 	}
 
 }
