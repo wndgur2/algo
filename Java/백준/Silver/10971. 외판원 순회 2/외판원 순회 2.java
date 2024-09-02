bad38b15bcd8361ab36aaf0f3e7ca1aa9fb91651
LeeJungHyeok <wndgur76@naver.com>
2024-09-02 14:02:46 +0900

    [Silver II] Title: 외판원 순회 2, Time: 76 ms, Memory: 13204 KB -BaekjoonHub

--- Java/백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ Java/백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -31,7 +31,8 @@ public class Main {
 		}
 		
 		// 순열
-		permutation(0, 0);
+		numbers[0] = 0;
+		permutation(1, 0);
 		
 		System.out.println(res);
 	}
@@ -40,9 +41,8 @@ public class Main {
 		if(depth == N-1) {
 			int w = edges[numbers[N-2]][numbers[N-1]];
 			if(w==0) return;
-			if(edges[numbers[N-1]][numbers[0]]==0) return;
-			w += edges[numbers[N-1]][numbers[0]];
-			cost+=w;
+			if(edges[numbers[N-1]][0]==0) return;
+			cost += w + edges[numbers[N-1]][numbers[0]];
 			
 			// 가능하다면 비용 비교
 			if(res > cost) res = cost;
@@ -50,10 +50,10 @@ public class Main {
 		}
 		
 		for (int i = depth; i < N; i++) {
-			if(depth>0 && edges[numbers[depth-1]][numbers[i]]==0) continue;
-			int w = depth==0? 0:cost + edges[numbers[depth-1]][numbers[i]];
+			if(edges[numbers[depth-1]][numbers[i]]==0) continue;
+			// depth=>i 연결
 			swap(i, depth);
-			permutation(depth+1, w);
+			permutation(depth+1, cost + edges[numbers[depth-1]][numbers[depth]]);
 			swap(i, depth);
 		}
 	}
