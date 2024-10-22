7b79d4eba1faa8a6b0e9a037ea6a6f003a2e9300
LeeJungHyeok <wndgur76@naver.com>
2024-10-22 22:47:21 +0900

    [Gold IV] Title: 두 배, Time: 300 ms, Memory: 43456 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Gold/31963. 두 배/두 배.java
+++ Java/백준/Gold/31963. 두 배/두 배.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,22 +9,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] numbers = new int[N];
+        long[] muls = new long[N]; // 2를 곱한 횟수
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        int res = 0;
+        long res = 0;
         // 5,000,000
-        // 250000
         for(int i=1; i<N; i++){
-            //log1000000 = 20
-            while(numbers[i-1]>numbers[i]) {
-                numbers[i]*=2;
-                res++;
-            }
+            long temp = (long)Math.ceil(log2(numbers[i-1]) - log2(numbers[i]));
+            muls[i] = Math.max(0, temp + muls[i-1]);
+            res+=muls[i];
         }
-
         System.out.println(res);
+    }
+
+    public static double log2(int num){
+        // log2num = ?
+        // 2^? = num
+        return Math.log(num)/Math.log(2);
     }
 }
